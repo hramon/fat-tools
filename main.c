b0a5302228c6508aa40aa9f8cdedbe7d75564737
hannes <remi5123@hotmail.com>
2014-08-29 14:46:50 +0200

    fixed linux segmentation fault due to editing read only string

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include "fat.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv){
+	char* filename = (char*)malloc((strlen("/test.txt")+1)*sizeof(char));
+	strcpy(filename,"/test.txt");
 	fat_object obj;
 	internal_file* file;
 	create_fat("test.img",FAT32,20*1024*1024);
 	read_fat(&obj,"test.img");
-	/*file = open_file_fat(&obj,"/test.txt");
+	file = open_file_fat(&obj,filename);
 	close_file_fat(&obj,file);
 
-	copy_file_to_fat(&obj,"fat.h","/fat.h");
+	free(filename);
 
-	copy_file_from_fat(&obj,"/fat.h","test.text");*/
+	filename = (char*)malloc((strlen("/fat.h")+1)*sizeof(char));
+	strcpy(filename,"/fat.h");
+
+	copy_file_to_fat(&obj,"fat.h",filename);
+
+	copy_file_from_fat(&obj,filename,"test.text");
 
 	close_fat(&obj);
 	return 0;
